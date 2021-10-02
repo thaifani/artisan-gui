32c29eb36152ef9fcd2468ad97aae600070e1b27
Zaher Thaifani <alzaher00@gmail.com>
2021-10-02 18:56:42 +0300

    adding my routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Infureal\Http\Controllers\GuiController;
 
-\Route::get('/', [GuiController::class, 'index'])
+\Route::get('/gui', [GuiController::class, 'index'])
     ->name('gui.index');
+\Route::get('/challenge', [GuiController::class, 'challenge'])
+    ->name('gui.challenge');
 
-\Route::post('{command}', [GuiController::class, 'run'])
+
+\Route::post('gui/{command}', [GuiController::class, 'run'])
     ->name('gui.run');
+\Route::post('challenge/{command}', [GuiController::class, 'run'])
+    ->name('gui.run_ch');
